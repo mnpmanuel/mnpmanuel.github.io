a3297ce75ac83bb9f9eb52eca6f67620886835e4
Manuel Palmeira <mnp.manuel@gmail.com>
2016-04-27 03:54:40 +0100

    newproject

---
+++ nobs/js/agency.js
@@ -0,0 +1,49 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+var completes = document.querySelectorAll(".complete");
+var toggleButton = document.getElementById("toggleButton");
+
+function toggleComplete() {
+  var lastComplete = completes[completes.length - 1];
+  lastComplete.classList.toggle('complete');
+}
+
+toggleButton.onclick = toggleComplete;
+
+function toggle() {
+    var ele = document.getElementById("toggleText");
+    var text = document.getElementById("displayText");
+    if(ele.style.display == "block") {
+            ele.style.display = "none";
+        text.innerHTML = "show";
+    }
+    else {
+        ele.style.display = "block";
+        text.innerHTML = "hide";
+    }
+} 
